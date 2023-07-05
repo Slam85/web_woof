5143884e0be7fdca39fea3105d05ae3c8b13eb5f
Montigny Gérald <131920955+Slam85@users.noreply.github.com>
2023-07-05 12:24:06 +0200

    db

--- database/migrations/2023_07_05_090532_create_post_like_table.php
+++ database/migrations/2023_07_05_090532_create_post_like_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('post_like', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('post_id');
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('post_like');
     }
 };
