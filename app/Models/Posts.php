5a48485df1049fe50770007240e7c4bbc1cc94ac
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-05 16:18:29 +0200

    MJ routes et controller

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Posts extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostsController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,29 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/index', [PostsController::class, 'index'])->name('home');
+
+
+// Routes pour les posts
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
 });
-
-Route::get('/createpost}', [PostsController::class, 'create'])
-    ->middleware(['auth', 'verified'])
-    ->name('create');
-
-Route::post('/createpost}', [PostsController::class, 'store'])
-    ->middleware(['auth', 'verified'])
-    ->name('store');
-
-Route::get('/editpost/{id}', [PostsController::class, 'edit'])
-    ->middleware(['auth', 'verified'])
-    ->name('edit');
-
-Route::put('/editpost/{id}', [PostsController::class, 'update'])
-    ->middleware(['auth', 'verified'])
-    ->name('update');
-
-Route::delete('/delete/{id}', [PostsController::class, 'destroy'])
-    ->middleware(['auth', 'verified'])
-    ->name('destroy');
-
-
-require __DIR__ . '/auth.php';
