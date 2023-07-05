12c74ae640ef7a3d61a906b1a2e469d3b88c7739
hel <127905102+candysphynx@users.noreply.github.com>
2023-07-05 15:31:31 +0200

    1st layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-</head>
-
-<body>
-    <header>
-        <nav>
-            <!-- login/logout, welcome, profile, bouton vers /createpost, logo et “woof, -username- !” -->
-        </nav>
-        <h1>@yield('title', 'LATEST POSTS')</h1>
-    </header>
-
-    <main>
-        @yield('content')
-    </main>
-    <footer>
-        &copy; WEBWOOF 2023 @ LeBocalAcademy © Diogo, Gérald, Jimmy, Héloïse
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('title', 'Latest Posts')
+@section('content')
+<section>
+    <div>{{--Post 1--}}<p>content</p>
+    </div>
+    <div>{{--Post 2--}}<p>content</p>
+    </div>
+    <div>{{--Post 3--}}<p>content</p>
+    </div>
+    <div>{{--Post 4--}}<p>content</p>
+    </div>
+</section>
+@endsection
+@section('footer')
