a53e68a25724d5c057b6bb81e054bf44b31afbea
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-05 12:28:22 +0200

    2nd commit

--- database/migrations/2023_07_05_090530_create_posts_table.php
+++ database/migrations/2023_07_05_090530_create_posts_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->foreign('post_like.post_id');
+            $table->string('title')->nullable();
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->bigInteger('user_id');
+            $table->integer('like')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
