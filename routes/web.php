f1d5a326bc5afe4d86d302c6224168b5819c7c69
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-05 14:05:35 +0200

    Route create post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +19,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/createpost}', [PostsController::class, 'create'])
+    ->middleware(['auth', 'verified'])
+    ->name('create');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::post('/createpost}', [PostsController::class, 'store'])
+    ->middleware(['auth', 'verified'])
+    ->name('store');
 
-require __DIR__.'/auth.php';
+Route::get('/editpost/{id}', [PostsController::class, 'edit'])
+    ->middleware(['auth', 'verified'])
+    ->name('edit');
+
+Route::put('/editpost/{id}', [PostsController::class, 'update'])
+    ->middleware(['auth', 'verified'])
+    ->name('update');
+
+Route::delete('/delete/{id}', [PostsController::class, 'destroy'])
+    ->middleware(['auth', 'verified'])
+    ->name('destroy');
+
+
+require __DIR__ . '/auth.php';
