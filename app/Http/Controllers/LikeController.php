9d9c900ca7be8c840907ae8fb5a43381a3dbf4ed
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-10 14:03:03 +0200

    MJ alerts et mise en page

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -10,20 +10,19 @@ class LikeController extends Controller
 {
     public function toggle(String $id)
     {
-        $post=Post::findOrFail($id);
+        $post = Post::findOrFail($id);
         $user_id = Auth::user()->id;
 
         $liked = Like::where('post_id', $post->id)
-                     ->where('user_id', $user_id)
-                     ->first();
+            ->where('user_id', $user_id)
+            ->first();
 
-    if (!$liked) {
-        $post->setLike();
-    } else {
-        $post->unlike();
+        if (!$liked) {
+            $post->setLike();
+            return redirect('/')->with('success', '👍 You like this.');
+        } else {
+            $post->unlike();
+            return redirect('/')->with('success', '👎 You hate this.');
+        }
     }
-
-    return redirect('/');
-    }
-
 }
